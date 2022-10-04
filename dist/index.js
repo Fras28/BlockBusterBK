07a9e140c7a42faea09f93d6a4680d955df52def
Franco Selvarolo <fselvarolo28@gmail.com>
2022-10-04 17:03:56 -0300

    with out nodemailer

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,7 @@ const blockbuster_routes_1 = __importDefault(require("./routes/blockbuster.route
 dotenv_1.default.config();
 (0, init_1.default)();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.urlencoded({ extended: true, limit: "50mb" }));
 app.use(body_parser_1.default.json({ limit: "50mb" }));
